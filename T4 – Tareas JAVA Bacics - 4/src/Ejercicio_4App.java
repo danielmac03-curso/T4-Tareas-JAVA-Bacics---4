a61390cccc457557301b16187f9a04db481c6d25
danielmac03-curso <danielmaciasgracia13@gmail.com>
2020-07-15 16:49:43 +0200

    Subido ejercicio 4 de T4 – Tareas JAVA Bacics

---
+++ T4 – Tareas JAVA Bacics - 4/src/Ejercicio_4App.java
@@ -0,0 +1,26 @@
+
+public class Ejercicio_4App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Escribe un programa Java que declare una variable entera N y asígnale un valor. A
+		continuación escribe las instrucciones que realicen los siguientes:*/		
+		int n = 25;
+		System.out.println("El valor de n es: " + n);
+
+		//Incrementar N en 77. 
+		n += 77;
+		System.out.println("El valor de n es: " + n);
+		
+		//Decrementarla en 3.
+		n -= 3;
+		System.out.println("El valor de n es: " + n);
+
+		//Duplicar su valor
+		n *= 2;
+		System.out.println("El valor de n es: " + n);
+		
+	}
+
+}
